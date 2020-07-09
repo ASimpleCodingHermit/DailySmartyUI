ef813a8b067a2cfb653957379494af3b02a79563
Joseph Chu <hotcupofjoe2013@gmail.com>
2020-07-09 16:44:24 -0400

    Revised the code from previous commits and branches

---
+++ src/components/logo.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class Logo extends Component {
+  render() {
+    return <img src="/assets/ds_circle_logo.png" className="logo-main" />;
+  }
+}
+
+export default Logo;
