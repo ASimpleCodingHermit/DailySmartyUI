07919d0a3dce042245c0e7d44cab54d1b17d4e58
Joseph Chu <hotcupofjoe2013@gmail.com>
2020-07-10 10:22:52 -0400

    Used axios to create get request

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,11 @@
+import { SET_RECENT_POSTS } from "./types";
+import axios from "axios";
+
 export function FetchRecentPosts() {
   return function (dispatch) {
-    //perform our request here
-    console.log("hello");
+    axios.get("https://api.dailysmarty.com/posts").then((response) => {
+      console.log("response from data", response.data);
+      dispatch({ type: SET_RECENT_POSTS, payload: response.data.posts });
+    });
   };
 }
